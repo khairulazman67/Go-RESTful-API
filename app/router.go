d65afcbba0dd87db677256687540e20397595308
khairulazman67 <azman.khairul67@gmail.com>
2024-06-09 12:00:12 +0700

    added integration testing

---
+++ app/router.go
@@ -0,0 +1,22 @@
+package app
+
+import (
+	"resfull_api/controller"
+	"resfull_api/exception"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func NewRouter(categoryController controller.CategoryController) *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/category/:categoryId", categoryController.FindById)
+	router.POST("/api/category", categoryController.Create)
+	router.PUT("/api/category/:categoryId", categoryController.Update)
+	router.DELETE("/api/category/:categoryId", categoryController.Delete)
+
+	router.PanicHandler = exception.ErrorHandler
+
+	return router
+}
